9dc29c305707ffe314e21f84747ebd86bed155cb
amermer <alper.mermer@adjust.com>
2021-10-31 20:37:35 +0000

    @alper removing redundant file deletion

--- docker-parallel-run.sh
+++ docker-parallel-run.sh
@@ -23,8 +23,6 @@ done
 
 result_codes=$(docker wait $(docker ps -q -f "name=th-regression"))
 
-rm results/reports/build_logs.txt
-
 for i in $(seq 1 $index); do
     docker logs th-regression${i} >> results/reports/build_logs.txt
     docker cp th-regression${i}:/app/results/reports results
